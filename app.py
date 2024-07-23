46b56c52f74f491ed4352512f5183cc1340fa035
ducdn03 <duongngocduc2003@gmail.com>
2024-07-23 15:33:05 +0700

    add support multiple stream in iperf test

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from openpyxl.chart import Reference, LineChart
 
 
 class BandwidthTest(tk.Tk):
-    def __init__(self, server='89.187.160.1', port=5201, duration=10, iterations=1, stream=1):
+    def __init__(self, server='89.187.162.1', port=5201, duration=10, iterations=1, stream=2):
         super().__init__()
         self.upl = []
         self.dowl = []
@@ -66,17 +66,20 @@ class BandwidthTest(tk.Tk):
             result_data = json.loads(result.stdout)
 
             if 'error' in result_data:
-                return self.test_results.append({'error': result_data['error']})
+                self.test_results.append({'error': result_data['error']})
             else:
                 for interval in result_data['intervals']:
-                    if reverse:
-                        self.test_results.append({
-                            'received_Mbps': interval['sum']['bits_per_second'] / 1e6,
-                        })
-                    else:
-                        self.test_results.append({
-                            'sent_Mbps': interval['sum']['bits_per_second'] / 1e6,
-                        })
+                    for i, stream in enumerate(interval['streams']):
+                        if len(self.test_results) <= i:
+                            self.test_results.append([])  # Ensure there is a list for each stream
+                        if reverse:
+                            self.test_results[i].append({
+                                'received_Mbps': stream['bits_per_second'] / 1e6,
+                            })
+                        else:
+                            self.test_results[i].append({
+                                'sent_Mbps': stream['bits_per_second'] / 1e6,
+                            })
         except subprocess.CalledProcessError as e:
             self.test_results.append({'error': str(e)})
         except json.JSONDecodeError:
@@ -99,24 +102,38 @@ class BandwidthTest(tk.Tk):
         print(end_time - start_time)
         self.progress['value'] = 100
         window.update_idletasks()
-        for res in self.test_results:
-            if 'sent_Mbps' in res:
-                self.upl.append(round(res["sent_Mbps"], 1))
-            elif 'received_Mbps' in res:
-                self.dowl.append(round(res["received_Mbps"], 1))
-            else:
-                error_cnt += 1
+
+        for i, stream_result in enumerate(self.test_results):
+            self.upl.append([])
+            self.dowl.append([])
+            for res in stream_result:
+                if 'sent_Mbps' in res:
+                    self.upl[i].append(round(res["sent_Mbps"], 1))
+                elif 'received_Mbps' in res:
+                    self.dowl[i].append(round(res["received_Mbps"], 1))
+                else:
+                    error_cnt += 1
+
         print(time.time() - end_time)
-        if error_cnt >= (self.iterations / 5):
+        if error_cnt >= (self.iterations * self.duration / 5):
             window.message = messagebox.showerror(title="test state", message="test failed")
             window.destroy()
             return
         window.message = messagebox.showinfo(title="test state", message="successfully test")
+
+        for i, upl_res in enumerate(self.upl):
+            if len(upl_res) != 0:
+                self.display_graph_plot(i=i, upl=self.upl[i], dowl=self.dowl[i])
+
+    def display_graph_plot(self, i, upl, dowl):
+        window = tk.Tk()
+        window.title(f"Stream {i + 1}")
+        window.geometry('800x600')
         fig = Figure(figsize=(5, 5), dpi=80)
         plot1 = fig.add_subplot(111)
 
-        plot1.plot(self.upl, label='Upload')
-        plot1.plot(self.dowl, label='Download')
+        plot1.plot(upl, label='Upload')
+        plot1.plot(dowl, label='Download')
         plot1.legend()
 
         canvas = FigureCanvasTkAgg(fig, master=window)
@@ -129,7 +146,7 @@ class BandwidthTest(tk.Tk):
 
         window.result_text = tk.Text(window, height=10, width=50)
         window.result_text.pack()
-        average_upl, average_dowl = self.average_bandwidth()
+        average_upl, average_dowl = self.average_bandwidth(upl=upl, dowl=dowl)
         if average_upl == 'error' and average_dowl == ' error':
             window.message = messagebox.showerror(title="average bandwidth state", message="average bandwidth error")
             window.destroy()
@@ -151,44 +168,49 @@ class BandwidthTest(tk.Tk):
 
     def export_bandwidth_test_to_excel(self):
         wb = openpyxl.Workbook()
-        sheet = wb.active
-        sheet.title = "Bandwidth Test Result"
-        sheet['A1'].value = "Upload (Mbps)"
-        sheet['B1'].value = "Download (Mbps)"
-        row = 2
-        for data in self.upl:
-            sheet[f'A{row}'].value = data
-            row += 1
+        for i in range(len(self.upl)):
+            sheet = wb.create_sheet(title=f"Stream {i + 1}")
+            sheet['A1'].value = "Upload (Mbps)"
+            sheet['B1'].value = "Download (Mbps)"
+            row = 2
 
-        row = 2
-        for data in self.dowl:
-            sheet[f'B{row}'].value = data
-            row += 1
+            for data in self.upl[i]:
+                sheet[f'A{row}'].value = data
+                row += 1
 
-        average_upl, average_dowl = self.average_bandwidth()
-        sheet['D5'].value = "Average Upload: "
-        sheet['D6'].value = "Average Download: "
+            row = 2
+            for data in self.dowl[i]:
+                sheet[f'B{row}'].value = data
+                row += 1
 
-        sheet['E5'].value = "{:.2f}".format(average_upl)
-        sheet['E6'].value = "{:.2f}".format(average_dowl)
+            average_upl, average_dowl = self.average_bandwidth(self.upl[i], self.dowl[i])
+            sheet['D5'].value = "Average Upload: "
+            sheet['D6'].value = "Average Download: "
 
-        # Create upload chart
-        upload_chart = LineChart()
-        upload_chart.title = "Upload Chart"
-        upload_chart.x_axis.title = "Times"
-        upload_chart.y_axis.title = "Mbps"
-        upload_values = Reference(sheet, min_col=1, max_col=1, min_row=2, max_row=len(self.upl) + 1)
-        upload_chart.add_data(upload_values, titles_from_data=True)
-        sheet.add_chart(upload_chart, "G2")
+            sheet['E5'].value = "{:.2f}".format(average_upl)
+            sheet['E6'].value = "{:.2f}".format(average_dowl)
 
-        # Create download chart
-        download_chart = LineChart()
-        download_chart.title = "Download Chart"
-        download_chart.x_axis.title = "Times"
-        download_chart.y_axis.title = "Mbps"
-        download_values = Reference(sheet, min_col=2, max_col=2, min_row=2, max_row=len(self.dowl) + 1)
-        download_chart.add_data(download_values, titles_from_data=True)
-        sheet.add_chart(download_chart, "G20")
+            # Create upload chart
+            upload_chart = LineChart()
+            upload_chart.title = "Upload Chart"
+            upload_chart.x_axis.title = "Times"
+            upload_chart.y_axis.title = "Mbps"
+            upload_values = Reference(sheet, min_col=1, min_row=2, max_row=row - 1)
+            upload_chart.add_data(upload_values, titles_from_data=True)
+            sheet.add_chart(upload_chart, "G2")
+
+            # Create download chart
+            download_chart = LineChart()
+            download_chart.title = "Download Chart"
+            download_chart.x_axis.title = "Times"
+            download_chart.y_axis.title = "Mbps"
+            download_values = Reference(sheet, min_col=2, min_row=2, max_row=row - 1)
+            download_chart.add_data(download_values, titles_from_data=True)
+            sheet.add_chart(download_chart, "G20")
+
+        # Remove default sheet created with workbook
+        if 'Sheet' in wb.sheetnames:
+            wb.remove(wb['Sheet'])
 
         files = [('Excel Files', '*.xlsx')]
         save_path = asksaveasfilename(filetypes=files)
@@ -196,11 +218,12 @@ class BandwidthTest(tk.Tk):
             wb.save(save_path)
             messagebox.showinfo(title="Export state", message="Export Completed")
 
-    def average_bandwidth(self):
-        if len(self.upl) == 0 or len(self.dowl) == 0:
+    @staticmethod
+    def average_bandwidth(upl, dowl):
+        if len(upl) == 0 or len(dowl) == 0:
             return 'error', 'error'
-        average_upl = sum(self.upl) / len(self.upl)
-        average_dowl = sum(self.dowl) / len(self.dowl)
+        average_upl = sum(upl) / len(upl)
+        average_dowl = sum(dowl) / len(dowl)
         return average_upl, average_dowl
 
     def configure_setting(self):
